84717bc8b051778415be144bf371288fa034a239
andreighervan <andrei.ghervan8@gmail.com>
2019-08-20 20:42:41 +0300

    add shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
