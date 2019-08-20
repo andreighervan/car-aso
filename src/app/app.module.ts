0a38eb23563a935b3fe16de2020d6ca098ef5e85
andreighervan <andrei.ghervan8@gmail.com>
2019-08-20 20:43:36 +0300

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SharedComponent } from './shared/shared.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SharedComponent
+    SharedComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
