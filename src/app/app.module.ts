6381b3c3b4d5d776430d3ba26b7cff8a0faef5b0
andreighervan <andrei.ghervan8@gmail.com>
2019-08-21 16:03:56 +0300

    add header to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SharedComponent } from './shared/shared.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SharedComponent,
-    HeaderComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { SharedComponent } from './shared.component';
+
+@NgModule({
+  declarations: [HeaderComponent, SharedComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[HeaderComponent, CommonModule]
+})
+export class SharedModule { }
