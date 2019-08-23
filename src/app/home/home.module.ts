a733973b1a3710e9c84321c03ccc6d430a63a61a
andreighervan <andrei.ghervan8@gmail.com>
2019-08-23 18:16:02 +0300

    add search component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider/slider.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [SliderComponent],
+  declarations: [SliderComponent, SearchComponent],
   imports: [
     CommonModule
   ]
