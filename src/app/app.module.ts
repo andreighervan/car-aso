bbc00817543419b1cbf6ec4b32133ae3ed0a9e96
andreighervan <andrei.ghervan8@gmail.com>
2019-08-31 11:55:38 +0300

    add testimonials component html and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HomeModule,
-    CarouselModule,
-    WavesModule,
-    NgbModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SliderComponent } from './slider/slider.component';
 import { SearchComponent } from './search/search.component';
 import { PopularCarsComponent } from './popular-cars/popular-cars.component';
+import { CtaComponent } from './cta/cta.component';
+import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
-  declarations: [SliderComponent, SearchComponent, PopularCarsComponent],
+  declarations: [HomeComponent, CtaComponent, PopularCarsComponent, SearchComponent, SliderComponent, TestimonialsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    CarouselModule,
+    WavesModule,
+    NgbModule
   ]
 })
 export class HomeModule { }
