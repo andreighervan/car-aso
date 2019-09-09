f6d6a65baf0203b71941e6f803637c0915dee675
andreighervan <andrei.ghervan8@gmail>
2019-09-09 19:34:03 +0300

    add posts component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,9 +9,10 @@ import { CtaComponent } from './cta/cta.component';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [HomeComponent, CtaComponent, PopularCarsComponent, SearchComponent, SliderComponent, TestimonialsComponent],
+  declarations: [HomeComponent, CtaComponent, PopularCarsComponent, SearchComponent, SliderComponent, TestimonialsComponent, PostsComponent],
   imports: [
     CommonModule,
     SharedModule,
